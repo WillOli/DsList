06c83aa85403ef73ada22604750f62d5412cc6b7
WillOli <William.ika@gmail.com>
2023-05-11 11:33:29 -0300

    Game search list id

--- src/main/java/com/WillDev/DsList/dto/GameMinDTO.java
+++ src/main/java/com/WillDev/DsList/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.WillDev.DsList.dto;
 
 import com.WillDev.DsList.entities.Game;
+import com.WillDev.DsList.projection.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -20,6 +21,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection){
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/com/WillDev/DsList/services/GameService.java
+++ src/main/java/com/WillDev/DsList/services/GameService.java
@@ -3,6 +3,7 @@ package com.WillDev.DsList.services;
 import com.WillDev.DsList.dto.GameDTO;
 import com.WillDev.DsList.dto.GameMinDTO;
 import com.WillDev.DsList.entities.Game;
+import com.WillDev.DsList.projection.GameMinProjection;
 import com.WillDev.DsList.repositories.GameRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,12 @@ public class GameService {
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(GameMinDTO::new).toList();
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByGameList(Long listId) {
+        List<GameMinProjection> games = gameRepository.searchByList(listId);
+        return games.stream().map(GameMinDTO::new).toList();
     }
 
 }
