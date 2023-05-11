4a7b27aca2eb1890abffcb2af3abeff8f12b3cd6
WillOli <William.ika@gmail.com>
2023-05-11 09:58:25 -0300

    GET game by id

--- src/main/java/com/WillDev/DsList/services/GameService.java
+++ src/main/java/com/WillDev/DsList/services/GameService.java
@@ -1,10 +1,14 @@
 package com.WillDev.DsList.services;
 
+import com.WillDev.DsList.dto.GameDTO;
 import com.WillDev.DsList.dto.GameMinDTO;
 import com.WillDev.DsList.entities.Game;
 import com.WillDev.DsList.repositories.GameRepository;
+
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -14,6 +18,14 @@ public class GameService {
 
     @Autowired
     public GameRepository gameRepository;
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    public GameDTO findById(@PathVariable Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return new GameDTO(result);
+    }
+
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
 
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
